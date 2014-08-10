7da828bc98e9ec70217b0ffe3b8a607f138f4e2f
Chris Stead <cmstead@gmail.com>
2014-08-10 09:45:37 -0700

    Refactored code into more isolated pieces for improved readability.

--- src/edwrdfr/core.clj
+++ src/edwrdfr/core.clj
@@ -4,11 +4,6 @@
 
 (def vowels "aeiou");
 
-(defn print-values [value-vector]
-  (when (seq value-vector)
-    (print (first value-vector) " ")
-    (recur (rest value-vector))))
-
 (defn pattern-remover [vowel-set astr]
   (str/replace astr
                (-> (str "[" vowel-set "]") (re-pattern))
@@ -19,12 +14,20 @@
     (str vowels "y")
     (str vowels)))
 
+(defn get-arg-set [args vowel-set]
+  (if (= vowels vowel-set)
+    (identity args)
+    (rest args)))
+
 (defn update-strings [args]
   (let [vowel-set (vowel-builder args)]
     (map (partial pattern-remover vowel-set)
-         (if (= vowels vowel-set)
-           (identity args)
-           (rest args)))))
+         (get-arg-set args vowel-set))))
+
+(defn print-values [value-vector]
+  (when (seq value-vector)
+    (print (first value-vector) " ")
+    (recur (rest value-vector))))
 
 (defn -main [& args]
   (let [updated-strings (update-strings args)]
